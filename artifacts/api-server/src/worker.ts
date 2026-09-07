e1fbb19c4a6fcfc4768100edcded158aaf7b5ccc
Aora-group-entreprise <hushengniaina@gmail.com>
2026-09-07 23:55:59 +0300

    fix(api): type direct Express Worker handler

--- artifacts/api-server/src/worker.ts
+++ artifacts/api-server/src/worker.ts
@@ -3,7 +3,7 @@ import app from "./app";
 import { cleanupExpiredStories } from "./jobs/story-cleanup";
 
 const server = app.listen(3000);
-const fetchHandler = httpServerHandler(server);
+const fetchHandler = httpServerHandler(server as never);
 
 export default {
   fetch: fetchHandler,
