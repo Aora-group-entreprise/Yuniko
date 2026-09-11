5cb8f00c6ccf286e0f7012fcb8c9e93007897ad4
Aora-group-entreprise <hushengniaina@gmail.com>
2026-09-11 12:21:00 +0300

    fix: load responsive foundation globally

--- artifacts/yuniko/src/main.tsx
+++ artifacts/yuniko/src/main.tsx
@@ -3,5 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./light-mode-fixes.css";
+import "./responsive.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
